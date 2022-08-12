2cabd0f0c2be503d3017734fe030b485483c8b8a
kawajiri <naoki19940317@yahoo.co.jp>
2022-08-12 16:12:03 +0900

    ニコ動：一括フェッチページ(nico/game)を実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     get 'search' => 'nico/search#index'
     post 'condition' => 'nico/condition#create'
     delete 'condition/:id' => 'nico/condition#destroy'
+    get 'game' => 'nico/game#index'
+    post 'game' => 'nico/game#create'
     get 'vocaloid' => 'nico/vocaloid#index'
     get 'utau' => 'nico/utau#index'
   end
