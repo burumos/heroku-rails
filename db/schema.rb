bceeb2179958d12f19f85c9d33d3cfe0691316fe
kawajiri <naoki19940317@yahoo.co.jp>
2022-09-19 14:11:43 +0900

    DBをsqliteに変更
    raspiに環境を移すため

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_12_043518) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "nico_conditions", force: :cascade do |t|
     t.integer "user_id"
     t.string "query"
